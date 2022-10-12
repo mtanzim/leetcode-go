eca6476925e488ea9ecccf5556b933ea067252ca
Tanzim Mokammel <mtanzim@gmail.com>
2022-10-12 18:13:36 -0400

    find the diff

---
+++ 389.find-the-difference.go/389.find-the-difference_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func Test_findTheDifference(t *testing.T) {
+	type args struct {
+		s string
+		t string
+	}
+	tests := []struct {
+		name string
+		args args
+		want byte
+	}{
+		{name: "basic", args: args{"abcd", "abcde"}, want: byte('e')},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findTheDifference(tt.args.s, tt.args.t); got != tt.want {
+				t.Errorf("findTheDifference() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
