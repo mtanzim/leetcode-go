f27cf11b91ab4a35f9a0f5afd95ac00a0ff81f1c
Tanzim Mokammel <mtanzim@gmail.com>
2022-11-18 10:15:00 -0500

    some kind of solution to longest palindromic, probably does not work

---
+++ 5.longest-palindromic-substring.go/5.longest-palindromic-substring.go
@@ -0,0 +1,76 @@
+package main
+
+/*
+ * @lc app=leetcode id=5 lang=golang
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (32.39%)
+ * Likes:    22632
+ * Dislikes: 1308
+ * Total Accepted:    2.2M
+ * Total Submissions: 6.8M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "cbbd"
+ * Output: "bb"
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ *
+ *
+ */
+
+// @lc code=start
+func isPalindrome(s string) bool {
+	front := 0
+	back := len(s) - 1
+	for front < back {
+		if s[front] != s[back] {
+			return false
+		}
+		front++
+		back--
+	}
+	return true
+}
+
+func longestPalindrome(s string) string {
+	seen := make(map[rune]int)
+	palindromes := []string{}
+	for i, v := range []rune(s) {
+		if prevLetterIdx, ok := seen[v]; ok {
+			runesToCheck := []rune(s)[prevLetterIdx:i+1]
+			if isPalindrome(string(runesToCheck)) {
+				palindromes = append(palindromes, string(runesToCheck))
+			}
+		}
+		seen[v] = i
+	}
+
+	return palindromes[0]
+}
+
+// @lc code=end
---
+++ 5.longest-palindromic-substring.go/5.longest-palindromic-substring_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "testing",
+			args: args{"babad"},
+			want: "bab",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
