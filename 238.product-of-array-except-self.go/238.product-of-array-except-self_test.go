eaeb98d2f3e44a927d039bd34e4c89d25ad32ae2
Tanzim Mokammel <mtanzim@gmail.com>
2022-11-23 21:02:53 -0500

    brute force of product of array

---
+++ 238.product-of-array-except-self.go/238.product-of-array-except-self_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_productExceptSelf(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "base case",
+			args: args{[]int{3, 2}},
+			want: []int{2, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := productExceptSelf(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("productExceptSelf() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
