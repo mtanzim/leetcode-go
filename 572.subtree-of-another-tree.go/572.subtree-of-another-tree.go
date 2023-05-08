0ae5d85dbbf8060502158835e8ec625c91f8d666
Tanzim Mokammel <mtanzim@gmail.com>
2023-05-08 19:56:35 -0400

    this one was a bit trickier

---
+++ 572.subtree-of-another-tree.go/572.subtree-of-another-tree.go
@@ -0,0 +1,78 @@
+package main
+
+/*
+ * @lc app=leetcode id=572 lang=golang
+ *
+ * [572] Subtree of Another Tree
+ *
+ * https://leetcode.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (45.91%)
+ * Likes:    7104
+ * Dislikes: 407
+ * Total Accepted:    650.6K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * Given the roots of two binary trees root and subRoot, return true if there
+ * is a subtree of root with the same structure and node values of subRoot and
+ * false otherwise.
+ *
+ * A subtree of a binary tree tree is a tree that consists of a node in tree
+ * and all of this node's descendants. The tree tree could also be considered
+ * as a subtree of itself.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the root tree is in the range [1, 2000].
+ * The number of nodes in the subRoot tree is in the range [1, 1000].
+ * -10^4 <= root.val <= 10^4
+ * -10^4 <= subRoot.val <= 10^4
+ *
+ *
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+	return isSameTree(root, subRoot) || isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+
+}
+
+// @lc code=end
